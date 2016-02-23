33c577a0205d412e1f4e40575828b6a02d10d1fc
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-02-23 14:26:57 -0600

    changes files for list-formatter

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,12 @@
 # This controller action should have a form for the user
-# to enter some Fahrenheit value.
+# to enter some comma separated list.
 MyApp.get "/" do
   erb :"homepage"
 end
 
 # This is the action that the form should submit to.
 # It does the calculation and returns a view that
-# displays the converted-to-Celsius temperature.
-MyApp.get "/celsius" do
-  erb :"to_celsius"
+# displays an HTML unordered list with the individual list items.
+MyApp.get "/list" do
+  erb :"list"
 end
